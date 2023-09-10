e1db1598297d53edda4a16a3e07772bf60350865
Abiodun Olorunda <34971027+Splendidabbey@users.noreply.github.com>
2023-09-10 20:06:17 +0100

    Added moment.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
+const moment = require('moment'); // Import the moment library
 
 app.get('/api', (req, res) => {
-  const slackName = req.query.slack_name || '';
+  const slackName = req.query.slack_nam e || '';
   const track = req.query.track || '';
   const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
-  const utcTime = new Date().toISOString();
+  const utcTime = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]'); // Format UTC time as specified
   const githubFileUrl = 'https://github.com/Splendidabbey/hng-stage1/blob/main/app.js';
   const githubRepoUrl = 'https://github.com/splendidabbey/hng-stage1';
-
+ 
   const response = {
     slack_name: slackName,
     current_day: currentDay,
-    utc_time: utcTime,
+    utc_time: utcTime, // Updated UTC time format
     track: track,
     github_file_url: githubFileUrl,
     github_repo_url: githubRepoUrl,
@@ -26,3 +27,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+  
